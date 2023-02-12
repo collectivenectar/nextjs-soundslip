a930d580ad5ce9e0494640a0fc18e5da57f48e7e
Jonathan Harvey <jonathanharvey22@gmail.com>
2023-02-12 09:52:57 -0800

    added interfaces, postgres test statement, restructured folders into src

---
+++ nextjs-soundslip/src/interfaces/Audiodata.ts
@@ -0,0 +1,11 @@
+export interface Audiodata {
+    id: Number,
+    created: String,
+    creator: Number,
+    title: String,
+    description: String,
+    downvotes: Number,
+    upvotes: Number,
+    tag: String,
+    visibility: Boolean
+}
